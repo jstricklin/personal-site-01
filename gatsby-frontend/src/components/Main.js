fe85e8577fa7f17c0bbb9034cb83f9c2b526dd93
Jose Stricklin <josestricklin@El-Diablo.local>
2018-11-28 20:18:19 -0700

    add resume

--- gatsby-frontend/src/components/Main.js
+++ gatsby-frontend/src/components/Main.js
@@ -6,6 +6,7 @@ import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 import menuScreen from '../images/menu-drawer-screen.png'
 import movieScreen from '../images/movie-buddy-screen.png'
+import resume from '../images/jose-stricklin-resume-11-18.pdf'
 
 class Main extends React.Component {
     render() {
@@ -16,7 +17,7 @@ class Main extends React.Component {
             <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
                 <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-                    <h2 className="major">About</h2>
+                    <h2 className="major">About Me</h2>
                     <span className="image main"><img src={pic01} alt="" /></span>
                     <p>Having come from a digital media and design background, much of my work has involved an emphasis on artistic and illustrative endeavors. I seek to continue evolving these creative and technical skills in order to contribute productively to the creation of a variety of fresh and innovative digital interactive experiences.</p>
 
@@ -25,7 +26,7 @@ class Main extends React.Component {
                 </article>
 
                 <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-                    <h2 className="major">Work</h2>
+                    <h2 className="major">Projects</h2>
                     {/*<span className="image main"><img src={pic02} alt="" /></span>*/}
                     <div className='card'>
                         <a target='_blank' rel='noopener noreferrer' href='https://github.com/jstricklin/menudrawer-front-end'>
@@ -60,8 +61,7 @@ class Main extends React.Component {
 
                 <article id="resume" className={`${this.props.article === 'resume' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
                     <h2 className="major">Resume</h2>
-                    <span className="image main"><img src={pic03} alt="" /></span>
-                    <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+                    <embed src={resume} width='550px' height='725px'/>
                     {close}
                 </article>
 
