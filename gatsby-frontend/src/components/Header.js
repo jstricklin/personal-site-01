4d275845169f50dfade8f93731fa248abd186c51
Jose Stricklin <josestricklin@El-Diablo.local>
2018-11-28 19:39:53 -0700

    add work content, update link names

--- gatsby-frontend/src/components/Header.js
+++ gatsby-frontend/src/components/Header.js
@@ -8,16 +8,16 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h3>Hello,</h3>
-                <h1>I'm Jose Stricklin</h1>
+                <h3>Hello, my name is</h3>
+                <h1>Jose Stricklin</h1>
                 <p>and I develop software.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('resume')}}>Resume</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
